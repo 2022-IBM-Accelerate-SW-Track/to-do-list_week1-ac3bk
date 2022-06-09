80c767ec695f8f0ba05b60e082a4696879126f1f
woodshyjean <woodshyjean@gmail.com>
2022-06-09 03:12:09 -0400

    followed the readme file

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,10 +1,30 @@
 import React, { Component } from 'react';
+import "./About.css";
   
 export default class About extends Component {
   render() {
     return (
       <div>
-        <p>Design your About me page </p> 
+        <div>
+           <div class="split left">
+             <div className="centered">
+               <img 
+                 className="profile_image"
+                 // Image goes here
+                 alt="Profile Pic"
+                 ></img>
+             </div>
+           </div>
+           <div className="split right">
+             <div className="centered">
+               <div className="name_title">Woodshy Jeanbaptiste</div>
+               <div className="brief_description">
+               My name is woodshy, i am a CET major at cuny City College of Technology. i am 19 years old. i like to play basketball, listen to music and explore outdoors. 
+              i want to learn software engineering so that i can play around with it and eventualy create something fun and exciting.
+               </div>
+             </div>
+           </div>
+         </div> 
       </div>
     )
   }
